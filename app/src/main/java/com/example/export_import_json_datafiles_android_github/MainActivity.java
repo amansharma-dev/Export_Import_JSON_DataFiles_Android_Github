6b2d5d0263945fd2132bcef412ec2a574f769580
Aman Sharma <amansharma.11@hotmail.com>
2019-06-22 20:25:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/export_import_json_datafiles_android_github/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.export_import_json_datafiles_android_github;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import com.example.export_import_json_datafiles_android_github.model.DataItem;
+import com.example.export_import_json_datafiles_android_github.sample.SampleDataProvider;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final  int REQUEST_PERMISSION_WRITE = 1001;
+    private  boolean permissionGranted;
+
+    private RecyclerView recyclerView;
+    List<DataItem> dataItemList = SampleDataProvider.dataItemList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DataItemAdapter adapter = new DataItemAdapter(this,dataItemList);
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setAdapter(adapter);
+
+        checkPermissions();
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        return (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state));
+    }
+
+    // Initiate request for permissions.
+    private boolean checkPermissions() {
+
+        if (!isExternalStorageReadable() || !isExternalStorageWritable()) {
+            Toast.makeText(this, "This app only works on devices with usable external storage",
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION_WRITE);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Handle permissions result
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_WRITE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    permissionGranted = true;
+                    Toast.makeText(this, "External storage permission granted",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "You must grant permission!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/export_import_json_datafiles_android_github/utils/JSONHelper.java
@@ -0,0 +1,4 @@
+package com.example.export_import_json_datafiles_android_github.utils;
+
+public class JSONHelper {
+}
